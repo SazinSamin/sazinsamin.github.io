ee2506da5845c1b87f6b801dac0c0b1a4ff171f6
SazinSamin <sazinsamin50@gmail.com>
2023-01-21 14:05:55 +0600

    change ferinheight to celcisu

--- prosthetic/js/fetch/fetchData.js
+++ prosthetic/js/fetch/fetchData.js
@@ -3,8 +3,8 @@ import preprocessing from "./preprocessing.js";
 const fetchObj = {};
 
 fetchObj.fetchDataLast10 = async () => {
-        // const res = await fetch("https://prosthetic-dasboard.onrender.com/last");
-        const res = await fetch("http://localhost:3000/last");
+        const res = await fetch("https://prosthetic-dasboard.onrender.com/last");
+        //const res = await fetch("http://localhost:3000/last");
 
         let json = await res.json();
         let data = [];
@@ -14,8 +14,8 @@ fetchObj.fetchDataLast10 = async () => {
 
 
 fetchObj.fetchDataAll = async () => {
-        // const res = await fetch("https://prosthetic-dasboard.onrender.com/lastfifty");
-        const res = await fetch("http://localhost:3000/lastfifty");
+        const res = await fetch("https://prosthetic-dasboard.onrender.com/lastfifty");
+        //const res = await fetch("http://localhost:3000/lastfifty");
         let json = await res.json();
         let data = [];
         data = preprocessing.makeArrayofData(json);
